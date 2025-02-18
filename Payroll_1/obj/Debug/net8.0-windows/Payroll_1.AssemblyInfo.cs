a39e45afe2964f08307cca687f13eb35b670a3c5
wols616 <wilber.rivas1@catolica.edu.sv>
2025-02-18 13:43:15 -0600

    sdfgsfdg

--- Payroll_1/obj/Debug/net8.0-windows/Payroll_1.AssemblyInfo.cs
+++ Payroll_1/obj/Debug/net8.0-windows/Payroll_1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Payroll_1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56f500520e65fddb4fbfac7a25384e6b12cedbb1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+340156dfe49ce45d580b5802c2e952769d07f56e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Payroll_1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Payroll_1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
